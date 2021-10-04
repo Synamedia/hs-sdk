bc11690818d546f937e276a633e63950c878d20e
Anat Verker <averker@synamedia.com>
2021-10-04 16:33:40 +0300

    implement persistent storage apis

--- src/api.js
+++ src/api.js
@@ -1,5 +1,28 @@
-export function init() {
+const communityManagerService = "hyperscale-community-manager:9092";
+
+let cachedAppStorage;
+
+async function loadAppStorageFromDb() {
+    const {tenant, sessionInfo} = window.diagnostics ? window.diagnostics() : {};
+    if (!tenant || !sessionInfo) {
+        return;
+    }
+    const {deviceId} = JSON.parse(sessionInfo);
+    if (!deviceId) {
+        return;
+    }
+    try {
+        const response = await fetch(`http://${communityManagerService}/devices/1.0/tenant/${tenant}/resourceId/${deviceId}`);
+        const data = await response.json();
+        cachedAppStorage = data?.appStorage;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export async function init() {
     console.log("hs-sdk init");
+    await loadAppStorageFromDb();
 }
 
 export function diagnostic() {
@@ -17,10 +40,28 @@ export function getSessionStorage(key) {
     return window.hsStorageGetItem(key);
 }
 
-export function setPersistentStorage(key, value) {
-
+export async function setPersistentStorage(key, value) {
+    const {tenant, sessionInfo} = window.diagnostics ? window.diagnostics() : {};
+    if (!tenant || !sessionInfo) {
+        return;
+    }
+    const {deviceId} = JSON.parse(sessionInfo);
+    if (!deviceId) {
+        return;
+    }
+    try {
+        const appStorage = {};
+        appStorage[key] = value;
+        const options = {method: "PUT", headers: {"Content-Type": "application/json"}, body: JSON.stringify({appStorage})};
+        const response = await fetch(`http://${communityManagerService}/devices/1.0/tenant/${tenant}/resourceId/${deviceId}`, options);
+        if (response?.status === 200) {
+            cachedAppStorage[key] = value;
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export function getPersistentStorage(key) {
-
+    return cachedAppStorage ? cachedAppStorage[key] : undefined;
 }
